f65cfc171999c799b8201abdef1508a89e921978
Robert Rouhani <robert.rouhani@gmail.com>
2012-09-22 17:29:21 -0400

    Collision working

--- src/tileset.py
+++ src/tileset.py
@@ -1,3 +1,4 @@
+import pygame
 from pytmx import tmxloader
 from pygame import surface
 
@@ -12,11 +13,24 @@ class Tileset(object):
         self.surface = surface.Surface((self.tiles.width * tw, self.tiles.height * th))
         
         #draw map
-        for l in xrange(0, len(self.tiles.tilelayers)):
-            for y in xrange(0, self.tiles.height):
-                for x in xrange(0, self.tiles.width):
-                    tile = gt(x, y, l)
-                    if tile: self.surface.blit(tile, (x*tw, y*th))
+        for y in xrange(0, self.tiles.height):
+            for x in xrange(0, self.tiles.width):
+                tile = gt(x, y, 0)
+                if tile: self.surface.blit(tile, (x*tw, y*th))
         
     def render(self, screen):
         screen.blit(self.surface, (0, 0))
+        
+    def get_walls(self):
+        walls = []
+        tw = self.tiles.tilewidth
+        th = self.tiles.tileheight
+        for y in xrange(0, self.tiles.height):
+            for x in xrange(0, self.tiles.width):
+                wall = self.tiles.getTileGID(x, y, 1)
+                if (wall): walls.append(pygame.Rect(x * tw, y * th, tw, th))
+        return walls
+                
+    def get_obstacles(self):
+        obstacles = []
+        return obstacles
