f219bab5eefc76c8664462c9c8516c6256452874
Robert Rouhani <robert.rouhani@gmail.com>
2012-09-22 13:48:20 -0400

    Setup for pydev/pygame.

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import pygame, os
+from pygame.locals import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480), HWSURFACE|DOUBLEBUF)
+    
+    stopevents = QUIT, KEYDOWN
+    while 1:
+        for e in pygame.event.get():
+            if e.type in stopevents:
+                return
+
+if __name__ == "__main__": main()
